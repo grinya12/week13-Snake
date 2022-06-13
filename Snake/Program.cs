d516b2d9dfcdb4ad44e85cd0bc2b95da8ad26507
Grigori Gritsenko <grissha2002@gmail.com>
2022-06-13 11:46:53 +0300

    Итоги

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -11,7 +11,6 @@ namespace Snake
 	{
 		static void Main(string[] args)
 		{
-			//Console.SetBufferSize(80, 25);
 
 			Walls walls = new Walls(80, 24);
 			walls.Draw();
@@ -21,7 +20,7 @@ namespace Snake
 			Snake snake = new Snake(p, 4, Direction.RIGHT);
 			snake.Draw();
 
-			FoodCreator foodCreator = new FoodCreator(80, 24, '$');
+			FoodCreator foodCreator = new FoodCreator(80, 23, '$');
 			Point food = foodCreator.CreateFood();
 			food.Draw();
 
@@ -47,6 +46,29 @@ namespace Snake
 					ConsoleKeyInfo key = Console.ReadKey();
 					snake.HandleKey(key.Key);
 				}
+			}
+
+			WriteGameOver();
+			Console.ReadLine();
+
+			static void WriteGameOver()
+			{
+				int xOffset = 25;
+				int yOffset = 8;
+				Console.ForegroundColor = ConsoleColor.DarkGreen;
+				Console.SetCursorPosition(xOffset, yOffset++);
+				WriteText("============================", xOffset, yOffset++);
+				WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+				yOffset++;
+				WriteText("Автор: Grigori Gritsenko", xOffset + 2, yOffset++);
+				WriteText("TTHK", xOffset + 12, yOffset++);
+				WriteText("============================", xOffset, yOffset++);
+			}
+
+			static void WriteText(String text, int xOffset, int yOffset)
+			{
+				Console.SetCursorPosition(xOffset, yOffset);
+				Console.WriteLine(text);
 			}
 
 		}
