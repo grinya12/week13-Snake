52117d0259fc39d4ed6fc50d138bd08dc1a6cac1
Grigori Gritsenko <grissha2002@gmail.com>
2022-06-13 10:57:00 +0300

    Класс "Препятствия". Полиморфизм

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -11,46 +11,31 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-
-			// Отрисовка рамочки
-			HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-			HorizontalLine downLine = new HorizontalLine(0, 78, 23, '+');
-			VerticalLine leftLine = new VerticalLine(0, 23, 0, '+');
-			VerticalLine rightLine = new VerticalLine(0, 23, 78, '+');
-			upLine.Draw();
-			downLine.Draw();
-			leftLine.Draw();
-            rightLine.Draw();
-
-			// Отрисовка точек			
+			VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
+			Draw(v1);
+	
 			Point p = new Point(4, 5, '*');
-			Snake snake = new Snake(p, 4, Direction.RIGHT);
-			snake.Draw();
+			Figure fsnake = new Snake(p, 4, Direction.RIGHT);
+			Draw(fsnake);
+			Snake snake = (Snake)fsnake;
 
-			FoodCreator foodCreator = new FoodCreator(80, 24, '$');
-			Point food = foodCreator.CreateFood();
-			food.Draw();
+			HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
 
-			while(true)
+			List<Figure> figures = new List<Figure>();
+			figures.Add(fsnake);
+			figures.Add(v1);
+			figures.Add(h1);
+
+			foreach(var f in figures)
             {
-				if(snake.Eat(food))
-                {
-					food = foodCreator.CreateFood();
-					food.Draw();
-				}
-				else
-                {
-					snake.Move();
-                }
-
-				Thread.Sleep(100);
-
-				if(Console.KeyAvailable)
-                {
-					ConsoleKeyInfo key = Console.ReadKey();
-					snake.HandleKey(key.Key);
-                }
+				f.Draw();
             }
 		}
+
+		static void Draw(Figure figure)
+        {
+			figure.Draw();
+        }
+
     }
 }
